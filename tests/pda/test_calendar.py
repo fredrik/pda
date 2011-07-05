8911eae19044c66d54511af7d8c6a78a5d16fadf
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2011-07-05 21:26:10 +0100

    make that work in your environment.

---
+++ tests/pda/test_calendar.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import unittest
+
+from pda.calendar import Calendar
+
+
+class TestCalendar(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_what(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
